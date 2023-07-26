8cb1db8fc3bcb17b6178caef8e0b565f3dfa8ed8
FBENH <125236000+FBENH@users.noreply.github.com>
2023-07-26 15:11:26 -0300

    Commit antes de mandar mail

--- Presentacion/ListadoDeViajes.aspx.cs
+++ Presentacion/ListadoDeViajes.aspx.cs
@@ -12,6 +12,7 @@ public partial class Default2 : System.Web.UI.Page
      
     protected void Page_Load(object sender, EventArgs e)
     {
+        //Para hacer, checkeo si hay usuario logueado
         if (!IsPostBack)
         {
             try
